4eb1142fb0cad84c53ea01f78021d10d3f496252
rafijadika <jadika.rafi@gmail.com>
2023-03-29 14:52:35 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int nilai;
+    cout << "masukan nilai siswa : "; cin >> nilai;
+
+    //asroni nilai rata rata 72
+    if ((nilai > 70) && (nilai <= 100))
+    {
+        cout << "diterima" << endl;
+    }
+
+    //lia nilai 80 rata rata
+    else if ((nilai >= 70) && (nilai <= 100))
+    {
+        cout << "diterima" << endl;
+    }
+
+    //joko nilai 65 rata rata
+    else if ((nilai >= 70) && (nilai <= 100))
+    {
+        cout << "diterima" << endl;
+    }
+
+}
